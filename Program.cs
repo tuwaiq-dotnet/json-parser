2510e7cb49120857f5ecb7a87581d0fed195f018
Younes Alturkey <younes.alturkey@tuwaiq.edu.sa>
2021-04-22 20:24:06 +0300

    added README.md

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Tuwaiq .NET Bootcamp | JSON Parser Project
+ * 
+ * Team Members
+ * Younes Alturkey
+ * Abdulrahman Bin Maneea
+ * Abdullah Albagshi
+ * Ibrahim Alobaysi
+ */
+
+using System;
+
+namespace JSONParser
+{
+    class Program
+    {
+        // Driver method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
