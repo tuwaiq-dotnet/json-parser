7b927f4481c59219e72e2dbdda294a83b146578e
AWManeea <Abdulrahman.Bin.Maneea@tuwaiq.edu.sa>
2021-04-23 17:10:44 +0300

    add(class): Input.cs

--- Tokenization/Input.cs
+++ Tokenization/Input.cs
@@ -19,35 +19,11 @@ namespace JSONParser
         private readonly int length;
         private int position;
         private int lineNumber;
+
         //Properties
-        public int Length
-        {
-            get
-            {
-                return this.length;
-            }
-        }
-        public int Position
-        {
-            get
-            {
-                return this.position;
-            }
-        }
-        public int NextPosition
-        {
-            get
-            {
-                return this.position + 1;
-            }
-        }
-        public int LineNumber
-        {
-            get
-            {
-                return this.lineNumber;
-            }
-        }
+        public int Length { get; }
+        public int Position { get; }
+        public int LineNumber { get; }
         public char Character
         {
             get
@@ -56,6 +32,7 @@ namespace JSONParser
                 else return '\0';
             }
         }
+
         public Input(string input)
         {
             this.input = input;
@@ -63,64 +40,24 @@ namespace JSONParser
             this.position = -1;
             this.lineNumber = 1;
         }
+
         public bool hasMore(int numOfSteps = 1)
         {
             if (numOfSteps <= 0) throw new Exception("Invalid number of steps");
             int t = (this.position + numOfSteps);
             return 0 <= t && t < this.length;
         }
-        public bool hasLess(int numOfSteps = 1)
-        {
-            if (numOfSteps <= 0) throw new Exception("Invalid number of steps");
-            return (this.position - numOfSteps) > -1;
-        }
-        //callback -> delegate
+
         public Input step(int numOfSteps = 1)
         {
-            if (this.hasMore(numOfSteps))
-                this.position += numOfSteps;
-            else
-            {
-                throw new Exception("There is no more step");
-            }
+            if (this.hasMore(numOfSteps)) this.position += numOfSteps;
+            else throw new Exception("There is no more step");
             return this;
         }
-        public Input back(int numOfSteps = 1)
-        {
-            if (this.hasLess(numOfSteps))
-                this.position -= numOfSteps;
-            else
-            {
-                throw new Exception("There is no more step");
-            }
-            return this;
-        }
-        public Input reset() { return this; }
+
         public char peek(int numOfSteps = 0)
         {
-            if (this.hasMore()) return this.input[this.NextPosition + numOfSteps];
-            return '\0';
-        }
-        //public bool hasMore(int numOfSteps=1) { return true; }
-
-        public int indexOf(char ch)
-        {
-            return this.input.IndexOf(ch);
-        }
-
-        public int lastIndexOf(char ch)
-        {
-            return this.input.LastIndexOf(ch);
-        }
-
-        public bool contains(char c)
-        {
-            return input.Contains(c);
-        }
-
-        public char lastCharacter()
-        {
-            return this.input[this.Length - 1];
+            return hasMore() ? this.input[this.Position + numOfSteps + 1] : '\0';
         }
     }
 }
