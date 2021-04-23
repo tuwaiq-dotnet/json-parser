343271df26a7afdd960ba89c04f7b17a2dd75996
Abdullah Albagshi <abdullah.albagshi@tuwaiq.edu.sa>
2021-04-24 00:47:19 +0300

    single Character handler completed

--- Tokenizer/Handlers/SingleCharHandler.cs
+++ Tokenizer/Handlers/SingleCharHandler.cs
@@ -1,4 +1,5 @@
-
+using System;
+using System.Collections.Generic;
 /*
  * Tuwaiq .NET Bootcamp
  * 
@@ -10,9 +11,43 @@
  *  Ibrahim Alobaysi
  */
 
-namespace JSONParser{
-    public class SingleCharHandler
+namespace JSONParser
+{
+    public class SingleCharHandler : Tokenizable
     {
-        
+        private List<Char> jsonCharacters = new List<Char> { ',', ':', '{', '}', '[', ']' };
+        public override bool tokenizable(Tokenizer t)
+        {
+            return this.jsonCharacters.Contains(t.input.peek());
+        }
+
+        public override Token tokenize(Tokenizer t)
+        {
+            int initPos = t.input.Position;
+            String val = t.input.step() + "";
+
+            switch (val)
+            {
+                case ",":
+                    return new Token(t.input.Position, t.input.LineNumber, TokenType.Comma, val);
+                    break;
+                case ":":
+                    return new Token(t.input.Position, t.input.LineNumber, TokenType.Colon, val);
+                    break;
+                case "{":
+                    return new Token(t.input.Position, t.input.LineNumber, TokenType.OpeningCurlyBracket, val);
+                    break;
+                case "}":
+                    return new Token(t.input.Position, t.input.LineNumber, TokenType.ClosingCurlyBracket, val);
+                    break;
+                case "[":
+                    return new Token(t.input.Position, t.input.LineNumber, TokenType.OpeningBracket, val);
+                    break;
+                case "]":
+                    return new Token(t.input.Position, t.input.LineNumber, TokenType.ClosingBracket, val);
+                    break;
+            }
+            throw new Exception("Unexpected Token");
+        }
     }
 }
