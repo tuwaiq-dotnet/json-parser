8cebb7b1bbbd712d73ee0869022719b65029c451
AWManeea <Abdulrahman.Bin.Maneea@tuwaiq.edu.sa>
2021-04-25 02:02:34 +0300

    fix: remove class conflicts

--- Program.cs
+++ Program.cs
@@ -19,17 +19,11 @@ namespace JSONParser
         // Driver method
         static void Main(string[] args)
         {
-
-
-
-
             JSON j = new JSON(@"{""k1"": ""Value"",""k2"":
             true,""k3"":null,""k4"":false,""k5"":
             [1,55,-3,""meow"",[null, {""younes"": null, ""abdul"": [1,2,3]}],{""Abdullah"": ""Is Awesome!"",""And"": 18,""Wa"": null}],""key6"":
             {""key7"": {""key8"":
             1}}}");
-
-
 
             // System.Console.WriteLine(j.Indent());
             Console.WriteLine(j.Root.getObject().getItem("k5").getArray().getItem(2));
